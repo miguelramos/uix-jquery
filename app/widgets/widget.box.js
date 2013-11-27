d269c25c0d76f00d9b9bfa81a9e5151679fdf09c
Miguel Ramos <miguel.marques.ramos@gmail.com>
2013-11-27 12:56:57 +0000

    Reindent.

--- app/widgets/widget.box.js
+++ app/widgets/widget.box.js
@@ -12,31 +12,31 @@
  * @link    https://www.websublime.com
  * @version 0.0.3
  */
-;(function($, undefined){
-	'use strict';
+ ;(function($, undefined){
+ 	'use strict';
 
-	$.widget( "uix.box", {
+ 	$.widget( "uix.box", {
 
 		// These options will be used as defaults
 		options: { 
-		  clear: null
+			clear: null
 		},
 
 		// Set up the widget
 		_create: function() {
 			var x = 1,
-				y = 2;
+			y = 2;
 
 			var z = x+y;
 		},
 
 		// Use the _setOption method to respond to changes to options
 		_setOption: function( key, value ) {
-		  switch( key ) {
-		    case "clear":
+			switch( key ) {
+				case "clear":
 		      // handle changes to clear option
 		      break;
-		  }
+		    }
 
 		  // In jQuery UI 1.8, you have to manually invoke the _setOption method from the base widget
 		  $.Widget.prototype._setOption.apply( this, arguments );
